41ca2faeaaf76d66fb0317fedeba053d92c97d8d
Trevor Aaron Robert Dcosta <63400592+dcostat04@users.noreply.github.com>
2021-11-05 19:23:15 +0530

    Nov 21 Long Challenge

---
+++ C/equalcoin.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int t,x,y,sum;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++){
+        scanf("%d%d",&x,&y);
+        if(y%2==0 && x%2==0){
+            printf("Yes\n");
+        }
+        else if(y%2!=0){
+            int z=(y/2)+1;
+            int k=z-(y/2);
+            x=x-(k*2);       //y is now equalized
+            if(x%2 == 0 && x>=0){
+                printf("Yes\n");
+            }
+            else{
+                printf("No\n");
+            }
+        }
+        else{
+            printf("No\n");
+        }
+    }
+    return 0;
+}
